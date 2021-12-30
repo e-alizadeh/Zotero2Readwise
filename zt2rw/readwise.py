8f349894c2a65dc3a0df5e60a1fd2ec32047f72f
ealizadeh <alizadeh.essi@gmail.com>
2021-12-30 17:02:14 -0500

    feat: Add `ZoteroAnnotationsNotes` to `Readwise` object. Define `convert_tags_to_readwise_format()`, `format_readwise_note()`, `convert_zotero_annotation_to_readwise_highlight()`
    Delete `format_highlights()`.

--- zt2rw/readwise.py
+++ zt2rw/readwise.py
@@ -1,8 +1,12 @@
+from dataclasses import dataclass
 from enum import Enum
-from typing import Dict, Optional, List
+from typing import Dict, List, Optional, Union
 
 import requests
-from dataclasses import dataclass
+from pyzotero.zotero import Zotero
+from zotero import ZoteroAnnotationsNotes
+
+from zt2rw.helper import sanitize_tag
 
 
 @dataclass
@@ -27,7 +31,7 @@ class ReadwiseHighlight:
     title: Optional[str] = None
     author: Optional[str] = None
     image_url: Optional[str] = None
-    source_url: Optional[str] = Category.articles
+    source_url: Optional[str] = Category.articles.name
     source_type: Optional[str] = None
     category: Optional[str] = None
     note: Optional[str] = None
@@ -41,34 +45,50 @@ class ReadwiseHighlight:
 
 
 class Readwise:
-    def __init__(
-        self,
-        access_token: str,
-    ):
-        self._token = access_token
+    def __init__(self, readwise_token: str, zotero_client: Zotero):
+        self._token = readwise_token
         self._header = {"Authorization": f"Token {self._token}"}
         self.endpoints = ReadwiseAPI
+        self.zot = ZoteroAnnotationsNotes(zotero_client)
 
-    def format_highlights(self) -> List[Dict]:
-        pass
-
-    def create_highlight(self, highlights: List[Dict]) -> None:
+    def create_highlights(self, highlights: List[Dict]) -> None:
         requests.post(
             url=self.endpoints.highlights,
             headers=self._header,
-            json={"highlights": highlights}
+            json={"highlights": highlights},
         )
 
+    @staticmethod
+    def convert_tags_to_readwise_format(tags: List[Dict]) -> str:
+        flattened_tags = [d_["tag"] for d_ in tags]
+        return " ".join([f".{sanitize_tag(tag.lower())}" for tag in flattened_tags])
 
+    def format_readwise_note(self, tags, comment) -> Union[str, None]:
+        tags = self.convert_tags_to_readwise_format(tags)
+        highlight_note = ""
+        if tags:
+            highlight_note += tags + "\n"
+        if comment:
+            highlight_note += comment
+        return highlight_note if highlight_note else None
 
-from json import load
-
-from zt2rw.zotero import get_zotero_client
-
-if __name__ == "__main__":
-    zot = get_zotero_client(library_id="8135490", api_key="buZVNxNzHcXx3VHdbO7HWwe2")
-
-    with open("./zt2rw/all_annotations.json", "r") as f:
-        annots = load(f)
-
-
+    def convert_zotero_annotation_to_readwise_highlight(
+        self, zotero_annot: Dict
+    ) -> ReadwiseHighlight:
+        data = zotero_annot["data"]
+        metadata = self.zot.get_item_metadata(zotero_annot)
+        highlight_note = self.format_readwise_note(
+            tags=data["tags"], comment=data["annotationComment"]
+        )
+        return ReadwiseHighlight(
+            text=data["annotationText"],
+            title=metadata["title"],
+            note=highlight_note,
+            author=metadata.get(metadata["creators"]),
+            category=Category.articles.name
+            if metadata["item_type"] != "book"
+            else Category.books.name,
+            highlighted_at=data["dateModified"],
+            source_url=metadata["source_url"] if metadata["source_url"] else None,
+            highlight_url=metadata["source_url"],
+        )
