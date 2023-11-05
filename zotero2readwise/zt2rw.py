349eaa6a6570e85ee6aea6121a8988f0c9d392e0
Noe Thalheim <2394624+noeleont@users.noreply.github.com>
2023-11-05 16:24:34 +0000

    feat(since): finish functionallity
    - get_all_zotero_items use since
    - refactor: retrieve_all_* to one function with item_type as parameter

--- zotero2readwise/zt2rw.py
+++ zotero2readwise/zt2rw.py
@@ -4,8 +4,6 @@ from zotero2readwise.readwise import Readwise
 from zotero2readwise.zotero import (
     ZoteroAnnotationsNotes,
     get_zotero_client,
-    retrieve_all_annotations,
-    retrieve_all_notes,
 )
 
 
@@ -33,17 +31,22 @@ class Zotero2Readwise:
         self.since = since
 
     def get_all_zotero_items(self) -> List[Dict]:
-        annots, notes = [], []
-        if self.include_annots:
-            annots = retrieve_all_annotations(self.zotero_client)
+            """
+            Retrieves all Zotero items of the specified types (notes and/or annotations) that were modified since the specified date.
 
-        if self.include_notes:
-            notes = retrieve_all_notes(self.zotero_client)
+            Returns:
+            A list of dictionaries representing the retrieved Zotero items.
+            """
+            items = []
+            if self.include_annots:
+                items.extend(self.retrieve_all("annotation", self.since))
 
-        all_zotero_items = annots + notes
-        print(f"{len(all_zotero_items)} Zotero items are retrieved.")
+            if self.include_notes:
+                items.extend(self.retrieve_all("note", self.since))
 
-        return all_zotero_items
+            print(f"{len(items)} Zotero items are retrieved.")
+
+            return items
 
     def run(self, zot_annots_notes: List[Dict] = None) -> None:
         if zot_annots_notes is None:
@@ -55,3 +58,26 @@ class Zotero2Readwise:
             self.zotero.save_failed_items_to_json("failed_zotero_items.json")
 
         self.readwise.post_zotero_annotations_to_readwise(formatted_items)
+    
+    def retrieve_all(self, item_type: str, since: int = 0):
+        """
+        Retrieves all items of a given type from Zotero Database since a given timestamp.
+
+        Args:
+            item_type (str): Either "annotation" or "note".
+            since (int): Timestamp in seconds since the Unix epoch. Defaults to 0.
+
+        Returns:
+            List[Dict]: List of dictionaries containing the retrieved items.
+        """
+        if item_type not in ["annotation", "note"]:
+            raise ValueError("item_type must be either 'annotation' or 'note'")
+
+        if since == 0:
+            print(f"Retrieving ALL {item_type}s from Zotero Database")
+        else:
+            print(f"Retrieving {item_type}s since last run from Zotero Database")
+
+        print("It may take some time...")
+        query = self.zotero_client.items(itemType={item_type}, since=since)
+        return self.zotero_client.everything(query)
