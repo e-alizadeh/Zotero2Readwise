c4eff4a179fae3195efa4a2660d60b459a79b657
JJJHolscher <jochem.holscher@gmail.com>
2024-10-07 19:00:05 +0200

    readme and dealing with readonly filesystems

--- zotero2readwise/__init__.py
+++ zotero2readwise/__init__.py
@@ -1,7 +1,10 @@
+import os
 from pathlib import Path
 
 __author__ = "Essi Alizadeh"
 __version__ = "0.4.5"
 
 TOP_DIR = Path(__file__).parent
-FAILED_ITEMS_DIR = TOP_DIR
+# If TOP_DIR is readonly os with nix,
+# then failed_zotero_items.json is written to the working directory instead.
+FAILED_ITEMS_DIR = TOP_DIR if os.access(TOP_DIR, os.W_OK) else Path(".")
